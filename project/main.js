0b07874596b32a0ff164b8e0cce0a3a90a1bb54e
Augusto Araujo <augustinho_darocha1311@hotmail.com>
2021-06-30 15:01:14 -0300

    javaScript [%] 80% feito

--- project/main.js
+++ project/main.js
@@ -1,17 +1,26 @@
-var desconto = 10
 var valorDoProduto = { prod1: 152.54, prod2: 180.92, prod3: 749.90 }
-
 function fonePhillips() {
+
+    var contaPorcentagem = 10/100*valorDoProduto.prod1
+    var resultado1 = contaPorcentagem-valorDoProduto.prod1
     var res = document.querySelector('#res')
-    res.innerHTML = `${valorDoProduto.prod1} ` - desconto
+    res.innerHTML = `${valorDoProduto.prod1} desconto: ${resultado1.toFixed(2)}`
 }
 
 function caixaSony() {
+    
+    var contaPorcentagem = 10/100*valorDoProduto.prod2
+    var resultado2 = contaPorcentagem-valorDoProduto.prod2
     var res = document.querySelector('#res')
-    res.innerHTML = `${valorDoProduto.prod2} ` - desconto
+    res.innerHTML = `${valorDoProduto.prod2} desconto: ${resultado2.toFixed(2)}`
 }
 
 function airApple() {
+    
+    var contaPorcentagem = 10/100*valorDoProduto.prod3
+    var resultado3 = contaPorcentagem-valorDoProduto.prod3
     var res = document.querySelector('#res')
-    res.innerHTML = `${valorDoProduto.prod3} ` - desconto
+    res.innerHTML = `${valorDoProduto.prod3} desconto: ${resultado3.toFixed(2)}`
 }
+/* desconto : 10/100 'X' valor do produto = resultado
+       resultado - valor do produto*/
